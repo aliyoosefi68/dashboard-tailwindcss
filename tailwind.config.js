796f6d67ec7600ecf38c465b64da45ef1e4ffd7a
Ali Yousefi <alifordevelpment@gmail.com>
2023-05-18 21:44:34 +0430

    responsive sidbars

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,7 @@ module.exports = {
     extend: {
       maxWidth: {
         xxs: "18rem",
+        lxxs: "15rem",
       },
       colors: {
         primary: {
@@ -14,6 +15,19 @@ module.exports = {
           gray: "#839bb1",
           lighter: "#fafbff",
         },
+      },
+      screens: {
+        sm: "640px",
+
+        md: "768px",
+
+        lg: "1024px",
+
+        xlg: "1180px",
+
+        xl: "1280px",
+
+        "2xl": "1536px",
       },
       fontFamily: {
         persian: ["Vazirmatn", ...defaultTheme.fontFamily.sans],
